efee65a25581d0ea73ccce0169ecfa3249afede1
Akshat <akshatindia2011@gmail.com>
2026-02-10 17:06:53 +0530

    Refactor calculateTotal using reduce

--- cart.js
+++ cart.js
@@ -1,36 +1,26 @@
-// function calculateTotal(cart) {
-//   return cart.reduce(
-//     (total, item) => total + item.price * item.quantity,
-//     0
-//   );
-// }
-// module.exports = calculateTotal;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function calculateTotal(cart) {
-  let total = 0;
-
-  for (let item of cart) {
-    total += item.price * item.quantity;
-  }
-
-  return total;
+  return cart.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
 }
-
 module.exports = calculateTotal;
+
+
+
+
+
+// function calculateTotal(cart) {
+//   let total = 0;
+
+//   for (let item of cart) {
+//     total += item.price * item.quantity;
+//   }
+
+//   return total;
+// }
+
+// module.exports = calculateTotal;
 
 
 
