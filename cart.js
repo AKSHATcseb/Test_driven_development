2b0e5374b09ccb6ef6d106d0ed3a30e08ade024b
Akshat <akshatindia2011@gmail.com>
2026-02-10 16:09:33 +0530

    Implement calculateTotal to pass tests

--- cart.js
+++ cart.js
@@ -1,4 +1,14 @@
+function calculateTotal(cart) {
+  let total = 0;
 
+  for (let item of cart) {
+    total += item.price * item.quantity;
+  }
+
+  return total;
+}
+
+module.exports = calculateTotal;
 
 
 
